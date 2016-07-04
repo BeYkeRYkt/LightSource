fc439e6545c961a4bc3ffc68e9aa8ad2b3b4d283
BeYkeRYkt <beykerykt@gmail.com>
2016-07-04 22:14:42 +0900

    flags: fix NullPointerException

--- src/ru/beykerykt/lightsource/items/flags/basic/PlayEffectExecutor.java
+++ src/ru/beykerykt/lightsource/items/flags/basic/PlayEffectExecutor.java
@@ -12,10 +12,10 @@ public class PlayEffectExecutor implements TickableFlagExecutor {
 		if (args.length >= 2) {
 			String effectName = args[0];
 			int data = Integer.parseInt(args[1]);
-			source.getLocation().getWorld().playEffect(source.getLocation(), Effect.getByName(effectName.toUpperCase()), data);
+			source.getLocation().getWorld().spigot().playEffect(source.getLocation(), Effect.valueOf(effectName.toUpperCase()), 0, data, 0, 0, 0, 0, 1, 1);
 		} else {
 			// default ?
-			source.getLocation().getWorld().playEffect(source.getLocation(), Effect.SMOKE, 0);
+			source.getLocation().getWorld().spigot().playEffect(source.getLocation(), Effect.SMOKE, 0, 0, 0, 0, 0, 0, 1, 1);
 		}
 	}
 

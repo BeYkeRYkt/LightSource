cfbffcf53038f99434b01229e83047ce699bf482
BeYkeRYkt <beykerykt@gmail.com>
2016-08-13 22:53:56 +0900

    flags: improvement FlagManager

--- src/ru/beykerykt/lightsource/items/flags/FlagManager.java
+++ src/ru/beykerykt/lightsource/items/flags/FlagManager.java
@@ -3,6 +3,11 @@ package ru.beykerykt.lightsource.items.flags;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import ru.beykerykt.lightsource.LightSourceAPI;
+
 public class FlagManager {
 
 	private Map<String, FlagExecutor> executors;
@@ -16,21 +21,33 @@ public class FlagManager {
 	}
 
 	public boolean registerFlag(String idKey, FlagExecutor executor) {
-		if (executors.containsKey(idKey))
+		if (executors.containsKey(idKey)) {
+			LightSourceAPI.sendMessage(Bukkit.getConsoleSender(), "This flag is already in the list.");
 			return false;
+		}
 		executors.put(idKey, executor);
+		LightSourceAPI.sendMessage(Bukkit.getConsoleSender(), "Registered new flag: " + ChatColor.YELLOW + idKey);
 		return true;
 	}
 
 	public boolean unregisterFlag(String idKey) {
-		if (!executors.containsKey(idKey))
+		if (!executors.containsKey(idKey)) {
+			LightSourceAPI.sendMessage(Bukkit.getConsoleSender(), "This id is not on the list.");
 			return false;
+		}
 		executors.remove(idKey);
+		LightSourceAPI.sendMessage(Bukkit.getConsoleSender(), "Removed flag: " + ChatColor.YELLOW + idKey);
 		return true;
 	}
 
-	public FlagExecutor getFlag(String id) {
-		FlagExecutor tag = executors.get(id);
-		return tag;
+	public FlagExecutor getFlag(String idKey) {
+		if (!hasFlag(idKey)) {
+			LightSourceAPI.sendMessage(Bukkit.getConsoleSender(), "This id is not on the list: " + ChatColor.YELLOW + idKey);
+		}
+		return executors.get(idKey);
+	}
+
+	public boolean hasFlag(String idKey) {
+		return executors.containsKey(idKey);
 	}
 }

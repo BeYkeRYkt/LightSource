61d1644b92f72e5fa2cb78d7b37333be170d3e3b
BeYkeRYkt <beykerykt@gmail.com>
2016-08-13 23:22:29 +0900

    sources: recode IgnoreEntityEntry for non-alive entities

--- src/ru/beykerykt/lightsource/sources/search/IgnoreEntityEntry.java
+++ src/ru/beykerykt/lightsource/sources/search/IgnoreEntityEntry.java
@@ -3,16 +3,12 @@ package ru.beykerykt.lightsource.sources.search;
 import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 
-import ru.beykerykt.lightsource.items.ItemSlot;
-
 public class IgnoreEntityEntry {
 	private Entity entity;
-	private ItemSlot slot;
 	private ItemStack itemStack;
 
-	public IgnoreEntityEntry(Entity entity, ItemStack itemStack, ItemSlot slot) {
+	public IgnoreEntityEntry(Entity entity, ItemStack itemStack) {
 		this.entity = entity;
-		this.slot = slot;
 		this.itemStack = itemStack;
 	}
 
@@ -24,17 +20,12 @@ public class IgnoreEntityEntry {
 		return itemStack;
 	}
 
-	public ItemSlot getSlot() {
-		return slot;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((entity == null) ? 0 : entity.hashCode());
 		result = prime * result + ((itemStack == null) ? 0 : itemStack.hashCode());
-		result = prime * result + ((slot == null) ? 0 : slot.hashCode());
 		return result;
 	}
 
@@ -62,9 +53,6 @@ public class IgnoreEntityEntry {
 				return false;
 			}
 		} else if (!itemStack.equals(other.itemStack)) {
-			return false;
-		}
-		if (slot != other.slot) {
 			return false;
 		}
 		return true;

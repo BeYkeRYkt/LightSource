22e26595eba297418c09094fcdf093410a93fb9d
BeYkeRYkt <beykerykt@gmail.com>
2016-08-17 16:12:21 +0900

    Introduce ConfigPath

---
+++ src/ru/beykerykt/lightsource/ConfigPath.java
@@ -0,0 +1,28 @@
+package ru.beykerykt.lightsource;
+
+public class ConfigPath {
+
+	public static class GENERAL {
+		public static String ADD_TO_ASYNC_LIGHTING_QUEUE = "add-to-async-lighting-queue";
+	}
+
+	public static class UPDATER {
+		public static String ENABLE = "updater.enable";
+		public static String REPO = "updater.repo";
+		public static String UPDATE_DELAY_TICKS = "updater.update-delay-ticks";
+		public static String VIEW_CHANGELOG = "view-changelog";
+	}
+
+	public static class SOURCES {
+		public static String UPDATE_DELAY_TICKS = "sources.update-delay-ticks";
+
+		public static class SEARCH {
+			public static String SEARCH = "sources.search.search-";
+			public static String SEARCH_PLAYERS = "sources.search.search-players";
+			public static String SEARCH_ENTITIES = "sources.search.search-entities";
+			public static String SEARCH_ITEMS = "sources.search.search-items";
+			public static String SEARCH_RADIUS = "sources.search.search-radius";
+			public static String SEARCH_DELAY_TICKS = "sources.search.search-delay-ticks";
+		}
+	}
+}

0d900635dda8c14d78231511aa1bc853c7167d19
BeYkeRYkt <beykerykt@gmail.com>
2016-08-18 18:14:49 +0900

    sources: improvement BurningSource

--- src/ru/beykerykt/lightsource/sources/BurningSource.java
+++ src/ru/beykerykt/lightsource/sources/BurningSource.java
@@ -2,6 +2,9 @@ package ru.beykerykt.lightsource.sources;
 
 import org.bukkit.entity.Entity;
 
+import ru.beykerykt.lightapi.LightAPI;
+import ru.beykerykt.lightapi.chunks.ChunkInfo;
+
 public class BurningSource extends OwnedSource {
 
 	public BurningSource(Entity entity, int lightlevel) {
@@ -10,6 +13,26 @@ public class BurningSource extends OwnedSource {
 
 	@Override
 	public boolean shouldExecute() {
-		return super.shouldExecute() && getOwner().getFireTicks() > 0;
+		boolean shoud = super.shouldExecute() && getOwner().getFireTicks() > 0;
+		if (!shoud) {
+			LightAPI.deleteLight(getOldLocation().getWorld(), getOldLocation().getBlockX(), getOldLocation().getBlockY(), getOldLocation().getBlockZ(), false);
+			LightAPI.deleteLight(getLocation().getWorld(), getLocation().getBlockX(), getLocation().getBlockY(), getLocation().getBlockZ(), false);
+			for (ChunkInfo info : LightAPI.collectChunks(getLocation().getWorld(), getLocation().getBlockX(), getLocation().getBlockY(), getLocation().getBlockZ())) {
+				LightAPI.updateChunk(info);
+			}
+		}
+		return shoud;
+	}
+
+	@Override
+	public void onUpdate() {
+		super.onUpdate();
+		if (getOldLocation().getBlockX() != getLocation().getBlockX() || getOldLocation().getBlockY() != getLocation().getBlockY() || getOldLocation().getBlockZ() != getLocation().getBlockZ()) {
+			LightAPI.deleteLight(getOldLocation().getWorld(), getOldLocation().getBlockX(), getOldLocation().getBlockY(), getOldLocation().getBlockZ(), false);
+			LightAPI.createLight(getLocation().getWorld(), getLocation().getBlockX(), getLocation().getBlockY(), getLocation().getBlockZ(), getLightLevel(), false);
+			for (ChunkInfo info : LightAPI.collectChunks(getLocation().getWorld(), getLocation().getBlockX(), getLocation().getBlockY(), getLocation().getBlockZ())) {
+				LightAPI.updateChunk(info);
+			}
+		}
 	}
 }
